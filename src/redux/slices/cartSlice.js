53e36e8e0437e8b429fc665f077e9c4459a13f2a
annakorkot <113845564+annakorkot@users.noreply.github.com>
2023-02-22 22:43:24 +0200

    item quantity ,new component for cart item

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -5,7 +5,24 @@ const cartSlice = createSlice({
     initialState : [],
     reducers:{
         addProduct(state,action){
-            state.push(action.payload);
+            const itemInCart =state.find((item) => item.id === action.payload.id);
+            if (itemInCart){
+                itemInCart.quantity++;
+            } else {
+                state.push({ ...action.payload ,quantity:1});
+            }
+        },
+        incrementQuantity(state,action){
+            const item = state.find((item)=>item.id === action.payload);
+            item.quantity++;
+        },
+        decrementQuantity(state,action){
+            const item = state.find((item) => item.id === action.payload);
+            if (item.quantity === 1) {
+              item.quantity = 1
+            } else {
+              item.quantity--;
+            }  
         },
         removeProduct(state,action){
             const index = state.indexOf(action.payload);
@@ -15,5 +32,5 @@ const cartSlice = createSlice({
 
 });
 
-export const {addProduct ,removeProduct} = cartSlice.actions;
+export const {addProduct ,removeProduct ,incrementQuantity,decrementQuantity} = cartSlice.actions;
 export default cartSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { productApi } from './apis/productsApi';
 import cartReducer from "./slices/cartSlice";
-import { addProduct , removeProduct } from "./slices/cartSlice";
+import { addProduct , removeProduct ,decrementQuantity,incrementQuantity} from "./slices/cartSlice";
 
 const appStore = configureStore({
     reducer: {
@@ -17,5 +17,5 @@ const appStore = configureStore({
 setupListeners(appStore.dispatch);
 
 export default appStore;
-export { addProduct ,removeProduct };
+export { addProduct ,removeProduct ,incrementQuantity,decrementQuantity };
 export { useFetchProductsQuery } from './apis/productsApi'
